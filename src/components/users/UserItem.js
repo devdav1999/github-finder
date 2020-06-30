770e38ce0e75438dc24f36e6ac2ee4ede394f74c
Jensen David <feudavid92@gmail.com>
2020-06-29 19:29:11 -0700

    initial commits starting from construction of search bar

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
+        return (
+            <div className="card text-center">
+               <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}} />
+               <h3>{login}</h3>
+               <div>
+                 <button><a href={html_url}>More</a></button>
+               </div>
+            </div>
+        )
+};
+
+UserItem.propTypes ={
+    user: PropTypes.object.isRequired
+}
+
+export default UserItem
