e98e3f1975e067ed3bb6430cf04a00961d9216ab
Jensen David <feudavid92@gmail.com>
2020-07-08 22:17:47 -0700

    added single user search

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 const UserItem = ({user: {login, avatar_url, html_url}}) => {
         return (
@@ -7,7 +8,11 @@ const UserItem = ({user: {login, avatar_url, html_url}}) => {
                <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}} />
                <h3>{login}</h3>
                <div>
-                 <button><a href={html_url}>More</a></button>
+                 <button>
+                   <Link to={`/user/${login}`}>
+                     More
+                  </Link>
+                 </button>
                </div>
             </div>
         )
